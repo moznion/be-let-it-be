37731aab8266d06a845873f136c05dcaf15999b0
moznion <moznion@mail.moznion.net>
2025-06-08 09:11:42 +0900

    Initial commit
    Signed-off-by: moznion <moznion@mail.moznion.net>

---
+++ lib/be_let_it_be.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "be_let_it_be/version"
+
+module BeLetItBe
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/be_let_it_be/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BeLetItBe
+  VERSION = "0.1.0"
+end
